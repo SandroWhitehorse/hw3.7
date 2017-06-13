efcbfce2776b139c1ea65d5b00c075c33e09e15f
Sandro Whitehorse <sg3802307@gmail.com>
2017-06-13 16:10:06 +0300

    Initial commit

---
+++ src/com/trylabs/Main.java
@@ -0,0 +1,16 @@
+package com.trylabs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num=100;
+        int i=0;
+	while (i<=15 ){
+            if (num%19==0) {i++;
+            System.out.println(num);
+            }
+         num++;
+    }
+
+    }
+}
